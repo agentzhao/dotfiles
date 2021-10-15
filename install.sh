884ed884075236accadeecee2b1a1d591e94fae8
Agentzhao <tan.hongzhao@dhs.sg>
2021-10-15 23:05:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ sudo apt install git
 # nvim
 curl -L https://github.com/neovim/neovim/releases/download/v0.5.1/nvim.appimage -o nvim
 chmod u+x nvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # config files
 git clone https://github.com/Agentzhao/dotfiles.git temp

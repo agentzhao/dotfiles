9d7254b74d5f5af7e94583a40bb7f7f372ec1274
Agentzhao <tan.hongzhao@dhs.sg>
2021-10-15 22:59:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,23 @@
 cd ~
-apt install curl git
+sudo apt install curl git
 
+# nvim
 curl -L https://github.com/neovim/neovim/releases/download/v0.5.1/nvim.appimage -o nvim
 chmod u+x nvim
 
+# config files
 git clone https://github.com/Agentzhao/dotfiles.git temp
 mv ~/temp/.bash_profile ~/
 mv -v ~/temp/* ~/.config
 rm -rf temp
 chown -R $USER .config
-
 echo "source ~/.bash_profile" >> .bashrc
 
-
+# hack font
 curl -L -O https://raw.githubusercontent.com/source-foundry/hack-linux-installer/master/hack-linux-installer.sh
 chmod +x hack-linux-installer.sh
 sudo mkdir ~/.local/share/fonts/
 ./hack-linux-installer.sh latest
+rm ./hack-linux-installer.sh
+
+rm ./install.sh

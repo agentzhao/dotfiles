c4964adf2a5e66d080d39fbd7006f976c9ca459c
Agentzhao <tan.hongzhao@dhs.sg>
2021-10-17 16:32:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,11 @@
 cd ~
-sudo apt install git
+sudo apt-get update && apt-get install git zsh nodejs -y
 
 # nvim
 curl -L https://github.com/neovim/neovim/releases/download/v0.5.1/nvim.appimage -o nvim
 chmod u+x nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-# config files
-git clone https://github.com/Agentzhao/dotfiles.git temp
-mv ~/temp/.bash_profile ~/
-mv -v ~/temp/* ~/.config
-rm -rf temp
-chown -R $USER .config
-echo "source ~/.bash_profile" >> .bashrc
 
 # hack font
 curl -L -O https://raw.githubusercontent.com/source-foundry/hack-linux-installer/master/hack-linux-installer.sh
@@ -23,5 +15,19 @@ sudo mkdir ~/.local/share/fonts/
 rm ./hack-linux-installer.sh
 fc-cache -f -v
 
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+
+# config files
+git clone https://github.com/Agentzhao/dotfiles.git temp
+mv -v ~/temp/* ~/.config
+rm -rf temp
+chown -R $USER .config
+echo "source ~/.config/.bash_profile" >> .bashrc
+echo "source ~/.config/.bash_profile" >> .zshrc
+
+# clean up
 rm .config/install.sh
 rm ./install.sh

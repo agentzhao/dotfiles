c02d33f4963268587be9e47e7dee0cd5d406d403
Agentzhao <tan.hongzhao@dhs.sg>
2021-10-18 21:36:48 +0800

    Update plugins.sh

--- plugins.sh
+++ plugins.sh
@@ -1,4 +1,9 @@
 # the fuck
 sudo apt update
-sudo apt install python3-dev python3-pip python3-setuptools
+sudo apt install python3-dev python3-pip python3-setuptools -y
 pip3 install thefuck --user
+
+# autojump
+sudo apt install autojump -y
+echo "source /usr/share/autojump/autojump.zsh" >> ~/.zshrc && source ~/.zshrc
+sudo chmod 755 /usr/share/autojump/autojump.bash

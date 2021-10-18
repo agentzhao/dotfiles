05e8f5f57caa3fffdc2a2f0acd755b22bead942a
Agentzhao <tan.hongzhao@dhs.sg>
2021-10-18 22:40:11 +0800

    Update plugins.sh

--- plugins.sh
+++ plugins.sh
@@ -7,3 +7,13 @@ pip3 install thefuck --user
 sudo apt install autojump -y
 echo "source /usr/share/autojump/autojump.zsh" >> ~/.zshrc && source ~/.zshrc
 sudo chmod 755 /usr/share/autojump/autojump.bash
+
+# asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+
+# auto env
+git clone git://github.com/inishchith/autoenv.git ~/.autoenv
+echo 'source ~/.autoenv/activate.sh' >> ~/.bashrc
+
+# bat
+sudo dpkg -i bat_0.18.3_amd64.deb  # adapt version number and architecture

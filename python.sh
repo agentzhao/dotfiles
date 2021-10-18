796859ee96deba363b683babfb8e3c9fd1393cda
Agentzhao <tan.hongzhao@dhs.sg>
2021-10-18 19:37:41 +0800

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,6 +1,6 @@
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.10
-sudo apt install python3-pip
+sudo apt install python3.9 -y
+sudo apt install python3-pip -y
 pip3 install pynvim

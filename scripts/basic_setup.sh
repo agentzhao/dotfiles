296e1b724cb7f9dbbd8fc8dccf3d0f051b7e1471
agentzhao <tan.hongzhao@dhs.sg>
2021-12-02 13:20:27 +0800

    Update basic_setup.sh

--- scripts/basic_setup.sh
+++ scripts/basic_setup.sh
@@ -10,6 +10,8 @@ sudo apt-get install -y nodejs
 # nvim
 sudo curl -L https://github.com/neovim/neovim/releases/download/v0.6.0/nvim.appimage -o /usr/local/bin/nvim
 sudo chmod +x /usr/local/bin/nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 

cdab8e62b4e043c5d80003783599efeb6b7f0335
agentzhao <tan.hongzhao@dhs.sg>
2021-12-02 20:24:22 +0800

    Update basic_setup.sh

--- scripts/basic_setup.sh
+++ scripts/basic_setup.sh
@@ -12,8 +12,8 @@ sudo curl -L https://github.com/neovim/neovim/releases/download/v0.6.0/nvim.appi
 sudo chmod +x /usr/local/bin/nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # hack font
 curl -L -O https://raw.githubusercontent.com/source-foundry/hack-linux-installer/master/hack-linux-installer.sh
